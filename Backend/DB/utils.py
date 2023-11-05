83c7bce03b38f1be1e03c2292a7d33699c933fb5
Cameron Magee <43101416+Cameron-Magee@users.noreply.github.com>
2023-11-05 19:31:46 +0000

    Adding code source

--- Backend/DB/utils.py
+++ Backend/DB/utils.py
@@ -1,2 +1,7 @@
+# ################################################
+# ######     Code Taken From
+# ######     https://stackoverflow.com/questions/47807446/convert-a-list-of-tuples-into-a-list-of-dict 
+# ################################################
+
 def fetchall_conversion(keys, car_data):
     return [dict(zip(keys, values)) for values in car_data]
